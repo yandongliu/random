ae6a96ac74af3c55169d922558356c49668da923
Yandong Liu <yandong@uber.com>
2017-01-02 13:47:56 -0800

    first commit

---
+++ compute_pi.py
@@ -0,0 +1,24 @@
+from math import sqrt
+from random import random
+
+MAX = 100000
+
+
+def is_in_quadrant(x, y, radius):
+    return sqrt(x * x + y * y) <= radius
+
+def compute_pi(radius=5):
+    count = 0
+    in_count = 0
+    while count < MAX:
+        x, y = sample_one_point(radius)
+        if is_in_quadrant(x, y, radius):
+            in_count += 1
+        count += 1
+    return float(in_count)/count * 4.0
+
+def sample_one_point(radius=5):
+    return (random() * radius, random() * radius)
+
+
+print compute_pi()
